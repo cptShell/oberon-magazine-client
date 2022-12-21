96fe63b1df3b2f4753056b815d1ff03814e51cd7
cptShell <repozheka@gmail.com>
2022-12-21 18:28:46 +0300

    fix: support panel

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { AsideBar } from '~/components/aside/aside';
 import { Main } from '~/components/main/main';
+import { Support } from '../support/support';
 import styles from './App.module.scss';
 
 export const App: FC<{}> = () => {
@@ -8,6 +9,7 @@ export const App: FC<{}> = () => {
     <div className={styles['App']}>
       <AsideBar />
       <Main />
+      <Support />
     </div>
   );
 };
--- src/components/support/support.tsx
+++ src/components/support/support.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import chat from '~/assets/img/icon/chat.svg';
+import styles from './support.module.scss';
 
 export const Support: FC<{}> = () => {
   return (
-    <div className="chat-container">
-      <img className="" src={chat} alt="support chat" />
+    <div className={styles['chat-container']}>
+      <img src={chat} alt="support chat" />
       <span>support</span>
     </div>
   );
